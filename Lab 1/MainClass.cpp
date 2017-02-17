49f5d7e117bd620cc2b2ca83b8f82f3b8a672314
mikesfox <mfox@ERC108-T>
2017-02-17 13:30:40 -0500

    test project from class

---
+++ Lab 1/MainClass.cpp
@@ -0,0 +1,61 @@
+#include "MainClass.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+MainClass::MainClass()
+{
+}
+
+
+MainClass::~MainClass()
+{
+}
+
+//forward declarations
+float computeTax(float sal, float taxRate);
+float computeAnnualNet(float salary, float taxRate);
+
+int main()
+{
+	const float taxRate = 0.2;					//assume 20% tax
+	
+	float salary;
+	while (true)
+	{
+		cout << "Enter Annual gross salary, 0 to exit: ";
+		cin >> salary;							//needs check for numeric
+		if (salary == 0)
+		{
+			break;								//done with calculations
+		}
+		cout << "Annual Salary = $" << salary << endl;
+		printf("Tax = $%.2f\n", computeTax(salary, taxRate));
+		float netSalary = computeAnnualNet(salary, taxRate);
+		float monthly = netSalary / 12;
+		monthly = roundf(monthly * 100) / 100;	//multiply / divide by 100 to effect rounding
+		printf("Net Salary = $%.2f\n", netSalary);
+		printf("Monthly = $%.2f", monthly);
+		cout << endl << endl;
+
+		//alternatively output this way, but you don't always get 2 decimal places
+		//cout << "Tax = $" << tax << endl;
+		//cout << "Net Salary = $" << netSal << "   Monthly = $" << monthly << endl << endl;
+		//
+	}
+}
+
+float computeTax(float sal, float taxRate)
+{
+	float tax = roundf(sal * 100 * taxRate) / 100;	//multiply / divide by 100 to effect rounding
+	return tax;
+}
+
+float computeAnnualNet(float salary, float taxRate)
+{
+	float netSal = salary - computeTax(salary, taxRate);
+	return netSal;
+}
